1391f2b5c09b3e32314ea2393e2c705320e92dd8
ssh-max <hossainsyedshabbir@gmail.com>
2026-05-17 23:57:54 +1000

    update config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 5000,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
